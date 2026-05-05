b4576a0499b66c9f1b18bd312e909150f31055be
Rabah ABED <35942821+rabahmilano@users.noreply.github.com>
2026-05-05 15:39:51 +0100

    feat: intégration de l'identité visuelle Planète Technologie et SEO
    - Ajout des nouveaux logos (complet et réduit) et du favicon
    - Configuration du nom de l'application dans themeConfig
    - Nettoyage du footer (retrait des liens du template)
    - Mise à jour des balises meta (titre et description) dans _app

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -96,12 +96,9 @@ const App = props => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
-        <meta
-          name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-        />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <title>{`${themeConfig.templateName} | Tableau de bord`}</title>
+        <meta name='description' content='Tableau de bord et espace de gestion pour Planète Technologie.' />
+        <meta name='keywords' content='Planète Technologie, PTech, Tableau de bord, Gestion, Admin' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
 
